b642572b1bc448930158bf6b49ddb94615ade1e1
gordondalos <gordondalos@gmail.com>
2016-01-23 17:26:17 +0600

    добавил свой шблон и добавил пейдж

---
+++ wp-content/themes/gordondalos/template-parts/content-home.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package gordondalos
+ */
+
+?>
+<?php the_content(); ?>
+
+
+
+
