78ddf7928c2cfd4389160625882860e170cceb81
gordondalos <gordondalos@gmail.com>
2016-01-23 20:11:03 +0600

    Добавили в пейдж билдер всекции айдищники

--- wp-content/themes/gordondalos/functions.php
+++ wp-content/themes/gordondalos/functions.php
@@ -116,6 +116,11 @@ add_action( 'widgets_init', 'gordondalos_widgets_init' );
 function gordondalos_scripts() {
 	wp_enqueue_style( 'gordondalos-style', get_stylesheet_uri() );
 
+	// подключил свои стили
+	wp_enqueue_style( 'gordondalos-gordondalos-style', get_template_directory_uri().'/css/gordondalos.css' );
+// подключил свои медия стили
+	wp_enqueue_style( 'gordondalos-media-style', get_template_directory_uri().'/css/media.css' );
+
 	wp_enqueue_script( 'gordondalos-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'gordondalos-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
@@ -150,3 +155,8 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Load tgm plugins.
+ */
+require get_template_directory() . '/tgm/gordondalos.php';
